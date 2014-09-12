bf0784acd5686248dca85929eb40667ad8a8f7b9
Adrian Cruz <drincruz@gmail.com>
2014-09-12 17:15:55 -0400

    Updated code structure. This closes #4

---
+++ simplebst/__init__.py
@@ -0,0 +1,57 @@
+"""
+Binary Search Tree
+
+"""
+
+class Node(object):
+    """
+    Data structure to represent a
+    node in a BST
+
+    """
+
+    def __init__(self, val):
+        """
+        Initialize the node with a value
+
+        Keyword arguments
+        val - Value to set the node to
+        """
+        self._value = val
+        self._left = None
+        self._right = None
+
+    def get_value(self):
+        """
+        Return the value of node
+
+        """
+        return self._value
+
+    def get_left(self):
+        """
+        Return the left child node
+
+        """
+        return self._left
+
+    def get_right(self):
+        """
+        Return the right child node
+
+        """
+        return self._right
+
+    def set_left(self, node):
+        """
+        Set the left child node
+
+        """
+        self._left = node
+
+    def set_right(self, node):
+        """
+        Set the right child node
+
+        """
+        self._right = node 
