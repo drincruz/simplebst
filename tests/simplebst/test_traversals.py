e27a14a5fa3b9aabde442a20787903b7b1a105c2
Adrian Cruz <drincruz@gmail.com>
2014-09-18 08:07:01 -0400

    added test for in_order_nodes() generator

--- tests/simplebst/test_traversals.py
+++ tests/simplebst/test_traversals.py
@@ -22,6 +22,11 @@ class TestSimplebstTraversals(object):
         assert 23 == self.root.get_value()
 
     def test_in_order_list(self):
+        """
+        Test in_order_list()
+        and make sure it returns an in-order list
+
+        """
         _expected_list = [5, 13, 23, 57, 103]
         _output_list = []
         
@@ -34,6 +39,16 @@ class TestSimplebstTraversals(object):
 
         assert len(_expected_list) == len(_output_list)
         assert _expected_list == _sorted_output
+
+    def test_in_order_nodes(self):
+        """
+        Test in_order_nodes() generator
+
+        """
+        _expected_values = [5, 13, 23, 57, 103]
+       
+        for i, val in enumerate(in_order_nodes(self.root)):
+            assert _expected_values[i] == val.get_value()
 
     def tearDown(self):
         pass
