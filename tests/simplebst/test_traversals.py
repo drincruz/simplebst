f819e44288966ab3b8909755b1a433033aaa5372
Adrian Cruz <drincruz@gmail.com>
2014-09-26 16:56:55 -0400

    added unit test for post_order_list

--- tests/simplebst/test_traversals.py
+++ tests/simplebst/test_traversals.py
@@ -4,12 +4,24 @@ test_traversals
 Tests for `simplebst` traversals.
 """
 
+
 from simplebst import Node
-from simplebst.traversals import in_order_list, in_order_nodes
+from simplebst.traversals import (
+    in_order_list, in_order_nodes,
+    post_order_list, post_order_nodes)
+
 from simplebst.utils import insert_node
 
 
 class TestSimplebstTraversals(object):
+    """
+      23
+     /  \
+    5   57
+     \    \
+     13   103
+
+    """
 
     def setUp(self):
         self.root = Node(23)
@@ -17,6 +29,9 @@ class TestSimplebstTraversals(object):
         insert_node(self.root, 5)
         insert_node(self.root, 13)
         insert_node(self.root, 103)
+
+    def tearDown(self):
+        pass
 
     def test_node_creation(self):
         assert 23 == self.root.get_value()
@@ -46,9 +61,26 @@ class TestSimplebstTraversals(object):
 
         """
         _expected_values = [5, 13, 23, 57, 103]
-       
+
         for i, val in enumerate(in_order_nodes(self.root)):
             assert _expected_values[i] == val.get_value()
 
-    def tearDown(self):
-        pass
+
+    def test_post_order_list(self):
+        """
+
+        :return:
+        """
+        _expected_list = [13, 5, 103, 57, 23]
+
+        _output_list = []
+
+        # Call post_order_list to test
+        post_order_list(self.root, _output_list)
+
+        # We just want to test the values
+        # so make a list from the list of objects
+        _post_order_output = [x.get_value() for x in _output_list]
+
+        assert len(_expected_list) == len(_output_list)
+        assert _expected_list == _post_order_output
