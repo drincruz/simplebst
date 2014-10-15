10d78e4d60a985896ec99a77ed3e3fb4bdd69847
Adrian Cruz <drincruz@gmail.com>
2014-10-15 18:18:14 -0400

    added tests for insert_node()
    This closes #57.

--- tests/simplebst/test_utils.py
+++ tests/simplebst/test_utils.py
@@ -50,11 +50,48 @@ class TestSimplebstUtils(object):
         pass
 
     def test_tree_height(self):
+        """
+        Test a random tree's height
+
+        :return:
+        """
         assert 2 == tree_height(self.tree0)
 
     def test_1_node_tree_height(self):
+        """
+        Test a single node tree (no children) height
+
+        :return:
+        """
         assert 0 == tree_height(self.tree1)
 
     def test_one_sided_tree(self):
+        """
+        Test a one-sided tree
+        (basically a linked list)
+
+        :return:
+        """
         assert 5 == tree_height(self.tree2)
 
+    def test_insert_node_lt(self):
+        """
+        Test insert_node inserts a less-than node to the
+        left child
+
+        :return:
+        """
+        tmp_tree = Node(10)
+        insert_node(tmp_tree, 5)
+        assert 5 == tmp_tree.get_left().get_value()
+
+    def test_insert_node_gt(self):
+        """
+        Test insert_node inserts a greater-than node to the
+        right child
+
+        :return:
+        """
+        tmp_tree = Node(10)
+        insert_node(tmp_tree, 15)
+        assert 15 == tmp_tree.get_right().get_value()
